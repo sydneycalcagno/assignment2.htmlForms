245a133be717d58f13d245fa6fd903cdc2d6fbd8
sydneycalcagno <89706903+sydneycalcagno@users.noreply.github.com>
2023-09-26 12:28:59 -0600

    second

--- index.js
+++ index.js
@@ -11,10 +11,31 @@ function runQuiz(){
     let prog = document.getElementById("progress");
     let player = document.getElementById("name");
     let tryAgain = document.getElementById("try");
+    let comH = document.getElementById("comH");
     
+    let playerPln = document.getElementById("playerPln"); //labels
+    let playerPlc = document.getElementById("playerPlc");
+    let playerSp = document.getElementById("playerSp");
+
+    let planetP = document.getElementById("pPlanet"); //inputs
+    let placeP = document.getElementById("pPlace");
+    let spP = document.getElementById("pSP");
+
+    let com = document.getElementById("com");
+
     tryAgain.style.display = "none";
     prog.style.display = "none";
     score.style.display = "none";
+
+    placeP.style.display = "none";
+    planetP.style.display = "none";
+    spP.style.display = "none";
+    com.style.display = "none";
+    comH.style.display = "none";
+
+    playerPln.style.display = "none";
+    playerPlc.style.display = "none";
+    playerSp.style.display = "none";
 
     btn.addEventListener("click", submitQuiz);
     form.addEventListener('submit', submitQuiz);
@@ -59,7 +80,21 @@ function runQuiz(){
         prog.style.display = "inline";
         score.style.display = "inline";
 
+        placeP.style.display = "inline";
+        planetP.style.display = "inline";
+        spP.style.display = "inline";
+        com.style.display = "block";
+        comH.style.display = "block";
+
+        playerPln.style.display = "inline";
+        playerPlc.style.display = "inline";
+        playerSp.style.display = "inline";
+
         score.innerHTML = player.value + "'s Score:";
+        playerPln.innerHTML = player.value + "'s Favorite Planet:";
+        playerPlc.innerHTML = "Where " + player.value + "'s From:";
+        playerSp.innerHTML = player.value + "'s Favorite South Park Character:";
+
 
         sum = one + two + three;
         prog.value = sum;
